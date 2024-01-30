325ebc1799daa04b225636f189db66a5a85107cd
krishnaraj-dev <krishnaraj.webdev@gmail.com>
2024-01-30 18:52:56 +0530

    fix: http-proxy-middleware package

--- apps/http-proxy-middleware/index.js
+++ apps/http-proxy-middleware/index.js
@@ -8,7 +8,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
  * Configure proxy middleware
  */
 const wsProxy = createProxyMiddleware({
-  target: 'ws://test.mosquitto.org:8080/mqtt',
+  target: 'http://ws.ifelse.io',
   // pathRewrite: {
   //  '^/websocket' : '/socket',        // rewrite path.
   //  '^/removepath' : ''               // remove path.
@@ -26,3 +26,15 @@ const server = app.listen(3000);
 server.on('upgrade', wsProxy.upgrade); // optional: upgrade externally
 
 console.log('[DEMO] Server: listening on port 3000');
+console.log('[DEMO] Opening: http://localhost:3000');
+
+
+/**
+ * Example:
+ * Open http://localhost:3000 in WebSocket compatible browser.
+ * In browser console:
+ * 1. `const socket = new WebSocket('ws://localhost:3000');`          // create new WebSocket
+ * 2. `socket.onmessage = function (msg) {console.log(msg)};`       // listen to socket messages
+ * 3. `socket.send('hello world');`                                 // send message
+ * >  {data: "hello world"}                                         // server should echo back your message.
+ **/
