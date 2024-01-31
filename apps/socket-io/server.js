4e98bbeae280ea604e356f135f443d9e3c62f1aa
krishnaraj-dev <krishnaraj.webdev@gmail.com>
2024-01-31 17:28:25 +0530

    fix: updating socket io

---
+++ apps/socket-io/server.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Listen for chat messages
+    socket.on('chat message', (msg) => {
+        console.log('Message:', msg);
+        // Broadcast the message to all connected clients
+        io.emit('chat message', msg);
+    });
+
+    // Listen for disconnect event
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
